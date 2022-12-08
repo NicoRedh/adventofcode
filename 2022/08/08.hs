26ad6839d01143bb0a9074627718c4ebbc96307c
Nicolas Roth <nicolas.roth@stud.uni-leipzig.de>
2022-12-08 12:46:44 +0100

    AoC day 8 part 2 in haskell

--- 2022/08/08.hs
+++ 2022/08/08.hs
@@ -2,6 +2,7 @@ import Data.Char (digitToInt)
 import Data.List (transpose)
 
 data Tree = Hidden Int | Visible Int deriving (Show, Eq)
+type  View = (Int, Int, Int, Int, Int) -- height left right up down
 
 readTrees :: [Char] -> [Tree]
 readTrees xs = [Hidden (digitToInt x) | x <- xs]
@@ -29,8 +30,56 @@ countVisible xs = sum $ map countVisibleRow xs
 
 parse :: [Char] -> [[Tree]]
 parse xs = map readTrees $ lines xs
---solve :: [Char] -> [[Int]]
+
+readViews :: [Char] -> [View]
+readViews xs = [(digitToInt x, 0, 0, 0, 0) | x <- xs]
+
+parseViews :: [Char] -> [[View]]
+parseViews xs = map readViews $ lines xs
+
+viewValue :: View -> Int
+viewValue (h, l, r, o, u) = l*r*o*u
+
+--beware: very dirty code below
+
+determineRowView :: Int -> [View] -> [View]
+determineRowView pos vs
+  | pos == length vs = vs
+  | otherwise = determineRowView (pos + 1) rs
+  where rs = fh ++ [determineTreeView (vs !! pos)] ++ lh
+        fh = take (pos) vs
+        lh = drop (pos + 1) vs
+        determineDist h ((he, le, re, oe, ue):ls)
+          | ls == [] = []
+          | h > he = (he,le,re,oe,ue) : determineDist h ls
+          | otherwise =  []
+        determineTreeView (h, l, r, o, u) = (h, if fh == [] then 0 else (length $ determineDist h $ reverse fh) + 1, if lh == [] then 0 else (length $ determineDist h lh) + 1, o, u)
+
+determineColView :: Int -> [View] -> [View]
+determineColView pos vs
+  | pos == length vs = vs
+  | otherwise = determineColView (pos + 1) rs
+  where rs = fh ++ [determineTreeView (vs !! pos)] ++ lh
+        fh = take (pos) vs
+        lh = drop (pos + 1) vs
+        determineDist h ((he, le, re, oe, ue):ls)
+          | ls == [] = []
+          | h > he = (he,le,re,oe,ue) : determineDist h ls
+          | otherwise =  []
+        determineTreeView (h, l, r, o, u) =(h, l, r, if fh == [] then 0 else (length $ determineDist h $ reverse fh) + 1, if lh == [] then 0 else (length $ determineDist h lh) + 1)
+
+calcAllViews :: [[View]] -> [[Int]]
+calcAllViews xs = map (map viewValue) xs
+
+findMaxView :: [[View]] -> Int
+findMaxView xs = maximum $  map maximum $ calcAllViews xs
+
 solve xs = countVisible $ map rowAndReverse $  transpose $ map rowAndReverse $ parse xs
+
+solve2 xs = findMaxView $ map (determineColView 0) $ transpose $ map (determineRowView 0) (parseViews xs)
+
+
 main = do
   input <- getContents
   print $ solve input
+  print $ solve2 input
